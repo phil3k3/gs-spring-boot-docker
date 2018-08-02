73e2a425410597443e35e77dbceecad2d4b525dd
Limbeck, Philip <philip.limbeck@automic.com>
2018-08-02 14:56:55 +0200

    added configurable message

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    @Value("${message}")
+    private String message;
+
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return this.message;
     }
 
     public static void main(String[] args) {
